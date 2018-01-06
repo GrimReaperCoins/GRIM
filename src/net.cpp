2684b1ab63a9528e3a057f2e9b31556f626f1ff5
GRIM Dev <32860043+GrimReaperCoins@users.noreply.github.com>
2018-01-05 19:48:22 -0800

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1229,6 +1229,8 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
+    {"reaper.rocks", "reaper.rocks"},
+    {"thec0de.com", "thec0de.com"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
